e78eba8d851e42bcbfea5cc769479e6bfe33d7e7
jungkihun <khkh0130@gmail.com>
2021-07-21 21:32:58 +0900

    jkh ip config

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express =require('express');
-const fs = require('fs');
 const app = express();
 app.get('/', function(req,res){
     res.send(`<html><body>
     <h1> jkh </h1>
     </body></html>`);
+})
+app.listen(3000,()=>{
+    conl
 })
